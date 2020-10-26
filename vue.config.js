391326d4e056f37ebdd4ecd894c05470489eff3c
flybbjcs70 <466704968@qq.com>
2020-10-26 16:23:07 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,20 +7,6 @@ module.exports = {
   css: {
     extract: false
   },
-  pages: {
-    index: {
-      entry: "src/pages/index/main.js",
-      template: "public/index.html",
-      filename: "index.html",
-      title: "Index Page"
-      // chunks: ["chunk-vendors", "chunk-common", "index"]
-    },
-    subpage: {
-      entry: "src/pages/subpage/main.js",
-      template: "public/subpage.html",
-      filename: "subpage/index.html"
-    }
-  },
   chainWebpack: config => {
     config.resolve.alias
       .set("@", resolve("src"))
